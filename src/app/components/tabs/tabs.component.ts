adc7a0ebb588b41ab0644fd75eeec65f6f15e271
deepak <107836346+10deepak10@users.noreply.github.com>
2023-12-19 22:21:23 +0530

    Tabs component

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from './tab/tab.component';
 
 @Component({
   selector: 'app-tabs',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tabs.component.html',
-  styleUrl: './tabs.component.scss'
+  styleUrl: './tabs.component.scss',
 })
 export class TabsComponent {
+  @ContentChildren(TabComponent) tabs!: QueryList<TabComponent>;
 
+  selectTab(tab: TabComponent) {
+    this.tabs.toArray().forEach((tab) => (tab.active = false));
+    tab.active = true;
+  }
 }
--- src/app/pages/assessment/assessment.component.ts
+++ src/app/pages/assessment/assessment.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { TabsComponent } from '../../components/tabs/tabs.component';
+import { TabComponent } from "../../components/tabs/tab/tab.component";
 
 @Component({
-  selector: 'app-assessment',
-  standalone: true,
-  imports: [],
-  templateUrl: './assessment.component.html',
-  styleUrl: './assessment.component.scss'
+    selector: 'app-assessment',
+    standalone: true,
+    templateUrl: './assessment.component.html',
+    styleUrl: './assessment.component.scss',
+    imports: [TabsComponent, TabComponent]
 })
 export class AssessmentComponent {
 
